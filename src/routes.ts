a0e70f9008ff2c0675ff566b1e1007f5a2ba2a65
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2024-09-03 16:19:19 -0300

    Publicando projeto...

---
+++ src/routes.ts
@@ -0,0 +1,16 @@
+//Gerenciador de rotas do express
+import { Router } from "express";
+import * as PlayerController from "./controllers/players-controller";
+import * as ClubsController from "./controllers/clubs-controller";
+
+const router = Router();
+
+router.get("/players", PlayerController.getPlayer);
+router.post("/players", PlayerController.postPlayer);
+router.delete("/players/:id", PlayerController.deletePlayer);
+router.get("/player/:id", PlayerController.getPlayerById);
+router.patch("/players/:id", PlayerController.updatePlayer);
+
+router.get("/clubs", ClubsController.getClubs);
+
+export default router;
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import createApp from "./app";
+
+const port = process.env.PORT;
+const app = createApp();
+
+app.listen(port, () => {
+    console.log(`🔥Server is running at port http://localhost:${port}🔥`);
+});
+
